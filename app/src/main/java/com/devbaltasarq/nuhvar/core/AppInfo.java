ef032dc9e9613f49fe685702d1521d612cfa456d
baltasarq <baltasarq@gmail.com>
2024-06-28 10:08:51 +0200

    Adds privacy policy and fixes docs

--- app/src/main/java/com/devbaltasarq/nuhvar/core/AppInfo.java
+++ app/src/main/java/com/devbaltasarq/nuhvar/core/AppInfo.java
@@ -7,7 +7,7 @@ package com.devbaltasarq.nuhvar.core;
 /** The version information for this app. */
 public class AppInfo {
     public static final String NAME = "nuHVAR";
-    public static final String VERSION = "v1.0.1 20240711";
+    public static final String VERSION = "v1.0.2 20240711";
     public static final String AUTHOR = "MILE Group";
     public static final String EDITION = "Friendly Dolphin";
 
